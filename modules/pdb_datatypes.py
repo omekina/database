c848940f0d9dd34fdfcba31d7425ebc69d58cc42
PloryCZ <czplory@gmail.com>
2022-02-21 18:44:38 +0100

    UPDATE save.py

--- modules/pdb_datatypes.py
+++ modules/pdb_datatypes.py
@@ -8,23 +8,41 @@ class dt0: # String
     def encode(data):
         raw_data = data.encode("utf-8", "ignore")
         return raw_data
+class dt1: # SInt
+    def decode(raw_data):
+        if raw_data[0] == "0":
+            data = int(raw_data[1:], base=2)
+        else:
+            data = -(int(raw_data[1:], base=2))
+        return data
+    def encode(data):
+        raw_data = bin(data)
+        if raw_data[0] == "-":
+            raw_data = raw_data[3:]
+        else:
+            raw_data = raw_data[2:]
+        return raw_data
 
 
 def decode(raw_data, datatype):
     try:
-        bytes_data = pdb_bitlogic.bits_to_octets(raw_data)
         if datatype == 0: # String
+            bytes_data = pdb_bitlogic.bits_to_octets(raw_data)
             decoded = dt0.decode(bytes_data)
+        elif datatype == 1: # SInt
+            decoded = dt1.decode(raw_data)
         else: raise Exception("datatype not supported") # Datatype not supported
         return decoded
     except Exception as exc: raise Exception("datatype_decode: " + str(exc))
     except: raise Exception("datatype_decode: error when decoding data")
 
-def encode(raw_data, datatype):
+def encode(raw_data):
     try:
-        if datatype == 0: # String
+        if type(raw_data) is str: # String
             bytes_data = dt0.encode(raw_data)
             encoded = pdb_bitlogic.octets_to_bits(bytes_data)
+        elif type(raw_data) is int: # SInt
+            bytes_data = dt1.encode(raw_data)
         else: raise Exception("datatype not supported") # Datatype not supported
         return encoded
     except Exception as exc: raise Exception("datatype_encode: " + str(exc))
--- save.py
+++ save.py
@@ -2,12 +2,52 @@ from modules import pdb_datatypes
 
 
 class session:
-    __slots__ = "flags", "columns", "columnNames", "data"
+    __slots__ = "flags", "columns", "columnNames", "data", "rows", "bitData", "bitLut", "bitLayout", "bitHead"
     def __init__(self, flags, columns, columnNames, data):
         self.flags = flags
         self.columns = columns
         self.columnNames = columnNames
         self.data = data
-    def parse_data(self):
-        data = ""
+        self.rows = []
+    
+
+    def construct_data(self): # Construct data bit stream
+        try:
+            data = ""
+            for i in range(len(self.columnNames)):
+                column = pdb_datatypes.encode(self.columnNames[i], 0)
+                self.columns[i].append(len(column))
+                data += column
+            for row in self.data:
+                if row[0]:
+                    row_bitencoded = pdb_datatypes.encode(row[0])
+                    self.rows.append([True, len(row_bitencoded])
+                    data += row_bitencoded
+                else:
+                    self.rows.append([False])
+            self.bit_data = data
+        except Exception as exc: raise Exception("save->data: " + str(exc))
+        except: raise Exception("save->data: error when constructing")
+    
+
+    def construct_lut(self): # Construct lut bit stream
+        try:
+            lut = ""
+            for row in self.rows:
+                if row[0]:
+                    pointer = bin(row[1])
+                    if pointer[:2] == "0b"
+                        pointer = pointer[2:]
+                    pointer_length = bin(len(pointer))[2:]
+                    if len(pointer_length) > 7: raise Exception("pointer too big")
+                    pointer_length = "0000000"[len(pointer_length):] + pointer_length
+                    lut += "1" + pointer_length + pointer
+                else:
+                    lut += "0"
+            if not (len(lut)/8).is_integer():
+                remainder = len(lut) % 8
+                lut += "".join(["0" for i in range(remainder)])
+            self.bitLut = lut
+        except Exception as exc: raise Exception("save->lut: " + str(exc))
+        except: raise Exception("save->lut: error when constructing")
 
